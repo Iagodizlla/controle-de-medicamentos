b6f2861a3411ff2d61704e922a258b62423f5f8e
Iagodizlla <iagoschlemper@gmail.com>
2025-05-21 16:36:28 -0300

    Implementado controlador inicial

---
+++ ControleDeMedicamentos/Controllers/ControladorInicial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace ControleDeMedicamentos.Controllers;
+
+[Route("/")]
+public class ControladorInicial : Controller
+{
+    [HttpGet]
+    public IActionResult PaginaInicial()
+    {
+        return View();
+    }
+}
